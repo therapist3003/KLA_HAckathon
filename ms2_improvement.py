d913fe6e48b89c62f9e3045f7165238b8d31ddaf
therapist3003 <75468335+therapist3003@users.noreply.github.com>
2024-04-15 21:21:51 +0530

    Update and rename ms2_post_intern.py to ms2_improvement.py
    Before improvement:
    TC1: Accuracy(100), Purity(84.62)
    Reason: A point is in circle only if distance is less than radius. I thought it would be in circle if distance is less than or equal to radius
    
    TC2: Accuracy(100), Purity(96.63)
    Reason: Same as TC1
    
    TC3: Accuracy(98.33), Purity(100)
    Reason: Even if all 4 corner points of a die are outside, if atleast one line segment intersects the wafer, then the die belongs to the wafer. Earlier, I considered only atleast one point lying within wafer to say it is a partial die
    
    TC4: Accuracy(96.57), Purity(100)
    Reason: Not yet figured out
    
    After improvement:
    TC1: Accuracy & Purity - 100
    TC2: Accuracy & Purity - 100
    TC3: Accuracy & Purity - 100
    
    TC4: Same (not improved)

--- ms2_improvement.py
+++ ms2_improvement.py
@@ -1,5 +1,39 @@
 import math
 import numpy as np
+from sympy import symbols, Eq, solve
+
+import matplotlib.pyplot as plt
+
+x, y = symbols('x y')
+
+def get_circle_eqn(radius, centre=(0,0)):
+    global x, y
+    h, k = centre
+    equation = Eq((x-h)**2 + (y-k)**2, radius**2)
+    return equation
+
+def get_line_eqn(point1, point2):
+    global x, y
+    x1, y1 = point1
+    x2, y2 = point2
+
+    if x2-x1 != 0:
+        slope = (y2-y1)/(x2-x1)
+    else:
+        slope = float('inf')
+
+    if slope != float('inf'):
+        equation = Eq(y-y1, slope*(x-x1))
+    else:
+        equation = Eq(x, x1)
+
+    return equation
+
+def is_point_on_line(point, line_end1, line_end2):
+    if line_end1[0] <= point[0] and point[0] <= line_end2[0] and line_end1[1] <= point[1] and point[1] <= line_end2[1]:
+        return True
+    else:
+        return False
 
 test_case_no = input('Enter testcase number: ')
 
@@ -22,19 +56,23 @@ ref_die_temp = (lines[3].split(':')[1])
 ref_die = (float(ref_die_temp.split(',')[0][1:]), float(ref_die_temp.split(',')[1][:-1]))
 # print(ref_die)
 
+circle_eqn = get_circle_eqn(radius)
+
 # Finds euclidean distance between two 2d coordinates
 def distance(p0, p1):
     return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
 # Checks whether an input coordinate is inside the wafer 
 def pointInCircle(point, rad = radius):
-    if distance((0,0), point) <= rad:
+    if distance((0,0), point) < rad:
         return True
     return False
 
 # Checks whether the die (partial/complete) is inside the wafer
 # IDEA: Even if one corner of the die is inside the wafer, then the die is inside the wafer 
+# New IDEA: If atleast one line segment intersects the circle, then die is inside wafer
 def isDieInCircle(die_centre):
+    global x, y
     # if not pointInCircle(die_centre):
     #     print('Point whose center not inside wafer: ', die_centre)
     die_top_left = (die_centre[0]-(die_width/2), die_centre[1]+(die_height/2))
@@ -42,10 +80,33 @@ def isDieInCircle(die_centre):
     die_bottom_left = (die_centre[0]-(die_width/2), die_centre[1]-(die_height/2))
     die_bottom_right = (die_centre[0]+(die_width/2), die_centre[1]-(die_height/2))
 
+    line_segments = [[die_top_left, die_top_right], [die_top_right, die_bottom_right], [die_bottom_right, die_bottom_left], [die_bottom_left, die_top_left]]
+
     if pointInCircle(die_top_left) or pointInCircle(die_top_right) or pointInCircle(die_bottom_left) or pointInCircle(die_bottom_right):
         return True
-    return False
+    
+    # If no corner point is inside the wafer, then check if any line segment intersects the wafer
+    real_solutions = []
+    for line_segment in line_segments:
+        line_eqn = get_line_eqn(line_segment[0], line_segment[1])
 
+        intersect_points = solve((line_eqn, circle_eqn), x, y)
+
+        for point in intersect_points:
+            if point[0].is_real and point[1].is_real:
+                real_solutions.append(point)
+        
+    points_inside_wafer = []
+
+    for point in real_solutions:
+        for line_segment in line_segments:
+            if is_point_on_line(point, line_segment[0], line_segment[1]):
+                points_inside_wafer.append(point)
+
+    if len(points_inside_wafer):
+        return True
+    return False
+    
 # Given the centre coordinate of the die w.r.t the wafer, it returns the lower left coordinate (w.r.t wafer) of the die
 def lowerLeftCorner(die_centre):
     return (float(die_centre[0])-(die_width/2), float(die_centre[1])-(die_height/2))
@@ -104,5 +165,23 @@ with open(file_path, 'w') as file:
     for key, value in res.items():
         file.write(f'{str(key)}: {str(value)}\n')
 
-# Wafer Plotting
+# Trying manual plotting
+fig,ax = plt.subplots()
+circle = plt.Circle((0,0), radius=wafer_dia/2, color='r', fill=False)
+ax.add_artist(circle)
+ax.set_aspect('equal')
+ax.set_xlim(-wafer_dia/2 - 10, wafer_dia/2 + 10)
+ax.set_ylim(-wafer_dia/2 - 10, wafer_dia/2 + 10)
 
+for key, val in res.items():
+    #ax.text(val[0]+die_width/2, val[1]+die_height/2, str(key), fontsize=6, ha='center', va='center')
+    die_rad = 0.2
+    die_index_str = ' '.join(map(str, key))
+    #ax.text(val[0]+die_width/2, val[1]+die_height/2, die_index_str, fontsize=6, ha='center', va='center', bbox=dict(boxstyle='circle', fc='white', ec='black', pad=0.3))
+
+    die_rect_coord = [val[0], val[1]]
+
+    rect = plt.Rectangle(die_rect_coord, die_width, die_height, fill=False, edgecolor='black', linewidth=1)
+    ax.add_patch(rect)
+
+plt.show()
